5a201a6ac0ed4f26c2673049f674e2d1da231d61
btokraks22 <bree.v.v.13@gmail.com>
2018-06-20 16:40:53 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -5,17 +5,21 @@ def first_student_by_index
   puts STUDENT_NAMES[0]
 end
 first_student_by_index
+
 def fourth_student_by_index
   puts STUDENT_NAMES[3]
 end
 fourth_student_by_index
+
 def last_student_by_index
   puts STUDENT_NAMES[5]
 end
-last_student_by_method
+last_student_by_index
+
 def first_student_by_method
 
 def last_student_by_method
+  
   # Write a solution that returns the first student using the built in .last method
 end
 
